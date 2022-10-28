d67098b09a443ae8408a9adbd388575bb949a9f6
Juls <rosas.julius18@gmail.com>
2022-10-28 16:03:08 +0800

    second commit

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
 function convert() {
-    let x = document.getElementById("firstNum").value;
-    const quotient = parseFloat(x) / 58.15 ;
+    let amount = document.getElementById("firstNum").value;
+    const quotient = parseFloat(amount) / 58.15 ;
 
     document.getElementById("converted").innerHTML = quotient;
     document.getElementById("unit").innerHTML = "USD";
 }
 
 function convert1() {
-    let x = document.getElementById("firstNum").value;
-    const quotient = parseFloat(x) / 0.40 ;
+    let amount = document.getElementById("firstNum").value;
+    const quotient = parseFloat(amount) / 0.4 ;
 
     document.getElementById("converted").innerHTML = quotient;
     document.getElementById("unit").innerHTML = "Yen";
 }
 
 function convert2() {
-    let x = document.getElementById("firstNum").value;
-    const quotient = parseFloat(x) / 0.041 ;
+    let amount = document.getElementById("firstNum").value;
+    const quotient = parseFloat(amount) / 0.041 ;
 
     document.getElementById("converted").innerHTML = quotient;
     document.getElementById("unit").innerHTML = "Won";
